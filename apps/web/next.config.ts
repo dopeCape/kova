7adf22edfd4b74da5325e7e4f0b5650210a49e10
dopeCape <tmank14319@gmail.com>
2025-08-13 03:26:26 +0530

    fix:simplfed Dockerfile for web

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,23 +1,5 @@
 import type { NextConfig } from "next";
-import path from "path";
-
 const nextConfig: NextConfig = {
   output: "standalone",
-  outputFileTracingRoot: path.join(__dirname, "../../"),
-
-  assetPrefix: undefined,
-  compress: true,
-  poweredByHeader: false,
-
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.resolve.modules = [
-      path.resolve(__dirname, "node_modules"),
-      path.resolve(__dirname, "../../node_modules"),
-      "node_modules"
-    ];
-
-    return config;
-  },
 };
-
 export default nextConfig;
